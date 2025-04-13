0ba4cc32b8e32c8ed99531226eddb386d49f97f0
chienchuanw <chienchuanwww@gmail.com>
2025-04-13 11:12:00 +0800

    modify: remove dropdown from burger menu

--- components/ui/navigation-menu.tsx
+++ components/ui/navigation-menu.tsx
@@ -47,19 +47,23 @@ const navigationMenuTriggerStyle = cva(
 
 const NavigationMenuTrigger = React.forwardRef<
   React.ElementRef<typeof NavigationMenuPrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Trigger>
->(({ className, children, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Trigger> & {
+    hideArrow?: boolean;
+  }
+>(({ className, children, hideArrow = false, ...props }, ref) => (
   <NavigationMenuPrimitive.Trigger
     ref={ref}
     className={cn(navigationMenuTriggerStyle(), "group", className)}
     {...props}
   >
     {children}{" "}
-    <FontAwesomeIcon
-      icon={faChevronDown}
-      className="relative top-[1px] ml-1 h-3 w-3 transition duration-300 group-data-[state=open]:rotate-180"
-      aria-hidden="true"
-    />
+    {!hideArrow && (
+      <FontAwesomeIcon
+        icon={faChevronDown}
+        className="relative top-[1px] ml-1 h-3 w-3 transition duration-300 group-data-[state=open]:rotate-180"
+        aria-hidden="true"
+      />
+    )}
   </NavigationMenuPrimitive.Trigger>
 ));
 NavigationMenuTrigger.displayName = NavigationMenuPrimitive.Trigger.displayName;
