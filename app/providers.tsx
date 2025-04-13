547720991dbdbd66c98aaa5257a513b43a20a6d7
chienchuanw <chienchuanwww@gmail.com>
2025-04-13 15:59:38 +0800

    modify: use tooltips for markdown tips display

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,12 +1,13 @@
-'use client';
+"use client";
 
-import React from 'react';
-import { SWRConfig } from '@/hooks/swr/swrConfig';
+import React from "react";
+import { SWRConfig } from "@/hooks/swr/swrConfig";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SWRConfig>
-      {children}
+      <TooltipProvider>{children}</TooltipProvider>
     </SWRConfig>
   );
 }
