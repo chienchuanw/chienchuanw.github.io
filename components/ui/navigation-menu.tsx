4395b1be43f737ef79cfb82d46f1263de3982fa6
chienchuanw <chienchuanwww@gmail.com>
2025-04-13 15:21:56 +0800

    layout: invert orientation of Admin button's arrow

--- components/ui/navigation-menu.tsx
+++ components/ui/navigation-menu.tsx
@@ -60,7 +60,7 @@ const NavigationMenuTrigger = React.forwardRef<
     {!hideArrow && (
       <FontAwesomeIcon
         icon={faChevronDown}
-        className="relative top-[1px] ml-1 h-3 w-3 transition duration-300 group-data-[state=open]:rotate-180"
+        className="relative top-[1px] ml-1 h-3 w-3 transition duration-300 rotate-180 group-hover:rotate-0 group-data-[state=open]:rotate-0"
         aria-hidden="true"
       />
     )}
